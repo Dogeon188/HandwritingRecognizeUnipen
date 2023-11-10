1a353d72f23c82f19720804e594008e0a406d2e9
Dogeon188 <dogcatdog188@gmail.com>
2023-11-11 01:08:31 +0800

    preprocessing

---
+++ utils/demonstration.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+gray_ascii = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+
+def print_digit(img_array):
+    shape_im = np.shape(img_array)
+    for i in range(shape_im[0]):
+        for j in range(shape_im[1]):
+            gray_scale = img_array[i][j]
+            gray_scale = (len(gray_ascii) - 1) - int(gray_scale * (len(gray_ascii) - 1) / 255)
+            print(gray_ascii[gray_scale], end='')
+        print('')
+
+def plot_digit(img_array, title: str = None):
+    plt.imshow(img_array, cmap='gray')
+    plt.title(title)
+    plt.show()
---
+++ utils/preprocess.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+import os
+
+raw_data_path = "data/curated"
+dataset_path = "data/unipen.tfrecord"
+
+def path2img(img_path):
+    img = tf.image.decode_png(tf.io.read_file(img_path), channels=1)
+    img = tf.reshape(img, [64, 64])
+    label_id = tf.strings.split(img_path, os.path.sep)[1]
+    label_id = tf.strings.to_number(label_id, out_type=tf.int32)
+    return img, {"code": label_id}
+
+def build_unipen_dataset():
+    return tf.data.Dataset.list_files(os.path.join(raw_data_path, "*/*.png")).map(path2img)
+
+def load_unipen_dataset():
+    if not os.path.exists(dataset_path):
+        dataset = build_unipen_dataset()
+        tf.data.Dataset.save(dataset, dataset_path)
+    return tf.data.Dataset.load(dataset_path)
