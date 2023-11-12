c54712f62abc519f0f8b0e14f7a1fa4370610aa6
Dogeon188 <dogcatdog188@gmail.com>
2023-11-12 17:02:21 +0800

    separate config, toggle normalize

---
+++ config.py
@@ -0,0 +1,9 @@
+# dataset
+no_cap = False  # if True, capital letters will be ignored
+normalize_dataset = False  # if True, the input image will be normalized to [0, 1]
+
+# model
+train_prop = 0.7  # proportion of training data
+batch_size = 32
+shuffle_buffer_size = 10000
+epochs = 10
