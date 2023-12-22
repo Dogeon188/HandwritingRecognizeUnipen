6ec6d402157a5abb81cfdfac7e275b608ca62b7e
Dogeon188 <dogcatdog188@gmail.com>
2023-12-22 13:51:28 +0800

    improve model & add normalize

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # dataset
 no_cap = False  # if True, capital letters will be ignored
-normalize_dataset = False  # if True, the input image will be normalized to [0, 1]
+normalize_dataset = True  # if True, the input image will be normalized to [0, 1]
 
 # model
 train_prop = 0.8  # proportion of training data
